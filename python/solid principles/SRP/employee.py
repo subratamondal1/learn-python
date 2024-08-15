b466a0604da40dee6effd24e9fe05e5ffa273055
Subrata Mondal <subratasubha2@gmail.com>
2024-08-15 11:09:11 +0530

    removed old codes

--- python/solid principles/SRP/employee.py
+++
@@ -1,7 +0,0 @@
-class Employee:
-    def __init__(self, name:str, salary:float) -> None:
-        self.name = name
-        self.salary = salary
-
-    def raise_salary(self, factor):
-        return self.salary * factor
--- python/solid principles/SRP/employee_storage.py
+++
@@ -1,3 +0,0 @@
-class EmployeeDB:
-    def save_employee(self, employee:object):
-        print(f"Employee: {employee.name} saved in the database.")
